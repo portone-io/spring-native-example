803b10a187fd1197bafd4cd351da2974558a8773
y0on2q <ltnscp9028@gmail.com>
2023-03-08 01:46:23 +0900

    Update to Spring Boot 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "2.7.10-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("org.springframework.boot") version "3.0.4"
+	id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "com.example"
@@ -10,8 +10,6 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
