781bf5e0698c46f17fa3bee5f2d34f345a7204d7
y0on2q <ltnscp9028@gmail.com>
2023-03-08 01:56:56 +0900

    Use spring-native plugin

--- README.md
+++ README.md
@@ -7,4 +7,9 @@ gradle bootRun
 
 # Run tests
 gradle test
+
+# Compile app
+gradle nativeCompile
+# Run compiled binary
+./build/native/nativeCompile/demo
 ```
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.0.4"
 	id("io.spring.dependency-management") version "1.1.0"
+    id("org.graalvm.buildtools.native") version "0.9.20"
 }
 
 group = "com.example"
